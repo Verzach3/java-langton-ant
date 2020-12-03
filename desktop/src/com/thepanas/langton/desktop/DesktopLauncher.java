1f26152534cd52eaed28c0bce791920e9ce1e972
Verzach3 <gabrielcorreacardenas@gmail.com>
2020-12-03 14:28:19 -0500

    restart project

--- desktop/src/com/thepanas/langton/desktop/DesktopLauncher.java
+++ desktop/src/com/thepanas/langton/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
-package com.thepanas.headbox.desktop;
+package com.thepanas.langton.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.thepanas.headbox.headbox_main;
+import com.thepanas.langton.langton_main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new headbox_main(), config);
+		new LwjglApplication(new langton_main(), config);
 	}
 }
